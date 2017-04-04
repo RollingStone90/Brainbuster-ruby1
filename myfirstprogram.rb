12e09838d399dc26f2694c6fee9e443776eaaf35
Jelani Hylton <Jelani.Hylton@yahoo.com>
2017-04-04 08:55:55 -0400

    first commit

---
+++ myfirstprogram.rb
@@ -0,0 +1,61 @@
+# Dog/program
+# puts "Is that a cat or dog"
+
+# animal = gets.chomp.downcase
+
+# if animal == "cat"
+# 	puts "meow"
+
+# 	elsif animal == "dog"
+# 	puts "woof"
+# 	else
+# 		puts "That is neither a cat no dog!"
+# end
+
+# puts "Number between 1 and 10"
+# num1= gets.chomp.to_i
+# if num1 == 5 
+# 	puts "We think alike"
+# else
+# 	puts "Wrong number"
+# end
+
+# puts "What was your Grade"
+# grade == gets.chomp.to_i
+# if grade >= 60
+# 	puts "you pass"
+# 	else
+# 		"you fail"
+# end
+
+
+#homework1
+# puts"What is your favorite Crayola crayon"
+# crayon= gets.chomp.upcase.reverse
+# puts crayon
+
+
+# # Homeowrk2
+# puts"How you feeling today"
+# feeling = gets.chomp
+# length = feeling.length
+# puts"That mood has #{length} characters."
+# puts"meow#{feeling}
+
+   homework3
+  puts"give me a number"
+  num1= gets.chomp.to_i
+  puts"Give me another number"
+  num2=gets.chomp.to_i
+  absval=(num1-num2).abs
+
+  if num1 >= num2
+  	quotient = num1/num2
+  	remainder =num1&num2
+  else
+  	quotient =num2/num1
+  	remainder = num2%num1
+  end
+
+  puts"The sum of your two numbers is equal
+
